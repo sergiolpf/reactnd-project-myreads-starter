6ef83f629887c7397cd4a92da0375a6aa345709a
SergioF <Slpf@002>
2018-04-05 15:43:33 +1200

    starting the process of deviding the comoponents, still have lots of work to do.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root'))
